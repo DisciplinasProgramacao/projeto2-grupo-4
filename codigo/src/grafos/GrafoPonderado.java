49fcf0156fe281e282f438e940705a76a155ce79
Arthur Avelar <aavelae@sga.pucminas.br>
2022-10-02 10:26:07 -0300

    Update GrafoPonderado.java
    Grafo ponderado metodos e variaveis

--- codigo/src/grafos/GrafoPonderado.java
+++ codigo/src/grafos/GrafoPonderado.java
@@ -1,7 +1,9 @@
 package grafos;
 
 import java.util.List;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import uteis.Util;
 import vertices.Vertice;
 
@@ -12,6 +14,17 @@ public class GrafoPonderado extends GrafoMutavel {
         
         return false;
     }
+    public boolean addAresta(int origem, int destino, int peso) {
+        
+        return false;
+    }
+    public boolean carregarGrafo(String path){
+
+        File arquivo = new File(path);
+
+        return false;
+
+    } 
 
     @Override
     public Grafo geraSubGrafo(List<Vertice> vertices_sub_grafo) {
